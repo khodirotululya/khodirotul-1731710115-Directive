cfaff42888fab786e8b4b0f264480156ed7b090f
khodirotululya <khodirotululya61@gmail.com>
2018-11-01 17:09:09 +0700

    first-commit

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, Renderer2 , OnInit, HostListener, HostBinding, Input} from '@angular/core';
+import {MockNgModuleResolver} from '@angular/compiler/testing';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+   @Input() defaultColor:string='transparent';
+
+   @Input('appBetterHighlight') highlightColor:string='blue';
+  //@HostBinding('style.backgroundColor')backgroundColor:string='transparent';
+  //@HostBinding('style.backgroundColor')backgroundColor:string=this.defaultColor;
+  @HostBinding('style.backgroundColor')backgroundColor:string;
+  
+  constructor(private elRef:ElementRef, private renderer:Renderer2) { }
+  ngOnInit(){
+   // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseover')mouseover(eventData:Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+  @HostListener('mouseleave')mouseleave(eventData:Event){
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+   //this.backgroundColor="transparent";
+   this.backgroundColor=this.defaultColor;
+  }
+
+}
